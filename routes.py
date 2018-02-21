8baec294905ac4742f11e908d613cd9c7420d76a
Ezequiel Leonardo Castaño <elcg@gmx.com>
2018-02-21 18:20:42 -0300

    Refactoring and reduce complexity
    - Isolate gui logic in a separate file (gui.py)
    - Use only 1function instead of 3 classes and 1 function
    - Replace class definition with instances of objects
    - Solve issue #3 - now internal links are open inside the app
    - Change convention from pamelCase to snake_case
    - Expose a function init_gui to the app.py
    - Improve use in already develop Flask apps
    
    TODO: Update PyQt from 4 to 5

--- routes.py
+++ routes.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template
-
-
-app = Flask(__name__)
-
+from flask import render_template
+from app import app
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/page2')
+def page2():
+    return render_template('page2.html')

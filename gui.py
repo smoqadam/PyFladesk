c44a019cfbfccf166ff6850178991816b2ef7874
Ezequiel Leonardo Castaño <elcg@gmx.com>
2018-02-21 21:09:17 -0300

    Refactor Imports
    - Use high level imports to reduce import statements
    
    Each module is used just once or twice so it's more convinient to use the full namespace

--- gui.py
+++ gui.py
@@ -1,10 +1,7 @@
 import sys
 import webbrowser
 
-from PyQt5.QtCore import QThread, QUrl
-from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets  import QApplication, QMainWindow
-from PyQt5.QtWebKitWidgets import QWebView, QWebPage
+from PyQt5 import QtCore, QtWidgets, QtWebKitWidgets, QtGui
 
 
 def init_gui(application, port=5000, width=300, height=400,
@@ -18,31 +15,32 @@ def init_gui(application, port=5000, width=300, height=400,
         if ROOT_URL not in ready_url:
             webbrowser.open(ready_url)
         else:
-            window.webView.load(QUrl(ready_url))
+            window.webView.load(QtCore.QUrl(ready_url))
 
     def run_app():
         application.run(port=port, threaded=True)
 
-    qtapp = QApplication(sys.argv)
+    qtapp = QtWidgets.QApplication(sys.argv)
 
-    webapp = QThread()
+    webapp = QtCore.QThread()
     webapp.__del__ = webapp.wait
     webapp.run = run_app
     webapp.start()
 
     qtapp.aboutToQuit.connect(webapp.terminate)
 
-    window = QMainWindow()
+    window = QtWidgets.QMainWindow()
     window.resize(width, height)
     window.setWindowTitle(window_title)
-    window.webView = QWebView(window)
+    window.webView = QtWebKitWidgets.QWebView(window)
     window.setCentralWidget(window.webView)
-    window.setWindowIcon(QIcon(icon))
+    window.setWindowIcon(QtGui.QIcon(icon))
 
-    window.webView.page().setLinkDelegationPolicy(QWebPage.DelegateAllLinks)
+    window.webView.page().setLinkDelegationPolicy(
+        QtWebKitWidgets.QWebPage.DelegateAllLinks)
     window.webView.page().linkClicked.connect(link_clicked)
 
-    window.webView.load(QUrl(ROOT_URL))
+    window.webView.load(QtCore.QUrl(ROOT_URL))
     window.show()
 
     return qtapp.exec_()

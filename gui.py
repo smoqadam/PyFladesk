ed857cf9a074c402148482a093afd593d3c51777
Ezequiel Leonardo Castaño <elcg@gmx.com>
2018-02-22 22:41:12 -0300

    Improve readability and add layout
    - Add Jinja layout HTML
    - Implementation of Mathias Ettinger Review
    
    Subclass the default Qt Classes and redefine methods instead of reassinging them
    
    Link: https://codereview.stackexchange.com/a/188124/161364

--- gui.py
+++ gui.py
@@ -1,46 +1,58 @@
 import sys
-import webbrowser
-
 from PyQt5 import QtCore, QtWidgets, QtWebKitWidgets, QtGui
 
 
-def init_gui(application, port=5000, width=300, height=400,
-             window_title="PyFladesk", icon="appicon.png"):
+class ApplicationThread(QtCore.QThread):
+    def __init__(self, application, port=5000):
+        super(ApplicationThread, self).__init__()
+        self.application = application
+        self.port = port
 
-    ROOT_URL = 'http://localhost:{}'.format(port)
+    def __del__(self):
+        self.wait()
 
-    # open links in browser from http://stackoverflow.com/a/3188942/1103397 :D
-    def link_clicked(url):
+    def run(self):
+        self.application.run(port=self.port, threaded=True)
+
+
+class WebView(QtWebKitWidgets.QWebView):
+    def __init__(self, root_url, parent=None):
+        super(WebView, self).__init__(parent)
+        self.root_url = root_url
+        self.page().setLinkDelegationPolicy(QtWebKitWidgets.QWebPage.DelegateAllLinks)
+        self.page().linkClicked.connect(self.__link_clicked)
+        self.load(QtCore.QUrl(self.root_url))
+
+    def __link_clicked(self, url):
+        """Open external links in browser and internal links in the webview"""
         ready_url = url.toEncoded().data().decode()
-        if ROOT_URL not in ready_url:
-            webbrowser.open(ready_url)
+        if self.root_url not in ready_url:
+            QtGui.QDesktopServices.openUrl(url)
         else:
-            window.webView.load(QtCore.QUrl(ready_url))
+            self.load(QtCore.QUrl(ready_url))
 
-    def run_app():
-        application.run(port=port, threaded=True)
 
-    qtapp = QtWidgets.QApplication(sys.argv)
+def init_gui(application, port=5000, width=300, height=400,
+             window_title="PyFladesk", icon="appicon.png", argv=None):
+    if argv is None:
+        argv = sys.argv
 
-    webapp = QtCore.QThread()
-    webapp.__del__ = webapp.wait
-    webapp.run = run_app
+    # Application Level
+    qtapp = QtWidgets.QApplication(argv)
+    webapp = ApplicationThread(application, port)
     webapp.start()
-
     qtapp.aboutToQuit.connect(webapp.terminate)
 
+    # Main Windows Level
     window = QtWidgets.QMainWindow()
     window.resize(width, height)
     window.setWindowTitle(window_title)
-    window.webView = QtWebKitWidgets.QWebView(window)
-    window.setCentralWidget(window.webView)
     window.setWindowIcon(QtGui.QIcon(icon))
 
-    window.webView.page().setLinkDelegationPolicy(
-        QtWebKitWidgets.QWebPage.DelegateAllLinks)
-    window.webView.page().linkClicked.connect(link_clicked)
+    # WebView Level
+    webView = WebView(f'http://localhost:{port}', window)
+    window.setCentralWidget(webView)
 
-    window.webView.load(QtCore.QUrl(ROOT_URL))
     window.show()
 
     return qtapp.exec_()

b95829a79f3f02d7f8e5a1b98432820956995d6f
Ezequiel Leonardo Castaño <elcg@gmx.com>
2019-04-10 21:07:03 -0300

    :pushpin: Add PyQtWebEngine as Dependency

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
 from setuptools import setup
 
 setup(name='PyFladesk',
-      version='1.0',
+      version='1.1',
       setup_requires=['setuptools-markdown'],
       long_description_markdown_filename='README.md',
       description='Create desktop application by using Flask and QtWebEngine',
       url="https://github.com/smoqadam/PyFladesk",
       author='​Saeed Moqadam, Ezequiel Castaño​',
-      author_email='​saeed.moqadam@gmail.com​, castanoezequielleonardo@gmail.com',
+      author_email='saeed.moqadam@gmail.com, castanoezequielleonardo@gmail.com',
       license='MIT',
       install_requires=[
           'flask',
           'pyqt5',
+          'pyqtwebengine',
       ],
       packages=['pyfladesk'],
       zip_safe=False,
